bce867bea5e5454f49f37f6cdf2d35051e470c1e
chambersgentile94-art <chambers.gentile94@gmail.com>
2026-05-14 23:23:08 -0300

    feat: agregar imagen principal al modelo de reporte

--- app/mobile/lib/models/animal_report.dart
+++ app/mobile/lib/models/animal_report.dart
@@ -10,6 +10,7 @@ class AnimalReport {
     required this.latitude,
     required this.longitude,
     this.approximateAddress,
+    this.mainImageUrl,
   });
 
   final String? id;
@@ -22,8 +23,18 @@ class AnimalReport {
   final double latitude;
   final double longitude;
   final String? approximateAddress;
+  final String? mainImageUrl;
 
   factory AnimalReport.fromMap(Map<String, dynamic> map) {
+    String? imageUrl;
+    final images = map['report_images'];
+    if (images is List && images.isNotEmpty) {
+      final firstImage = images.first;
+      if (firstImage is Map<String, dynamic>) {
+        imageUrl = firstImage['image_url'] as String?;
+      }
+    }
+
     return AnimalReport(
       id: map['id'] as String?,
       animalType: map['animal_type'] as String,
@@ -35,6 +46,7 @@ class AnimalReport {
       latitude: (map['latitude'] as num).toDouble(),
       longitude: (map['longitude'] as num).toDouble(),
       approximateAddress: map['approximate_address'] as String?,
+      mainImageUrl: imageUrl,
     );
   }
 
